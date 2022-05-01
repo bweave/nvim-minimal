2c7ac6e8776883ebf2ff057886e69e82b9416850
Brian Weaver <bdrums@gmail.com>
2022-05-01 16:21:01 +0000

    Bring over all the autocommands I expect to have

--- lua/settings.lua
+++ lua/settings.lua
@@ -78,7 +78,13 @@ for _, plugin in pairs(disabled_built_ins) do
 end
 
 -- Autocommands
-cmd([[autocmd BufWritePre * :%s/\s\+$//e]])  -- Remove whitespace on save
+cmd [[autocmd BufWritePre * :%s/\s\+$//e]]  -- Remove whitespace on save
+cmd 'autocmd VimResized * wincmd ='       -- automatically resize splits when resizing the window
+cmd 'autocmd FileType javascript iabbrev <buffer> wiplog console.log("WIPLOG",)<left>' -- wiplog JS
+cmd 'autocmd FileType javascriptreact iabbrev <buffer> wiplog console.log("WIPLOG",)<left>' -- wiplog JS
+cmd 'autocmd FileType ruby iabbrev <buffer> wiplog Rails.logger.debug "=" * 80<CR>Rails.logger.debug <CR>Rails.logger.debug "=" * 80<Up>' -- wiplog Ruby
+cmd 'autocmd FileType gitcommit iabbrev <buffer> co Co-authored-by: SOMEONE <HANDLE@users.noreply.github.com>' -- Co-authored-by
+cmd 'autocmd FileType php setlocal tabstop=4 shiftwidth=4 softtabstop=4' -- php
 
 -- Terminal
 cmd([[command! Term :botright vsplit term://$SHELL]])  -- Open a terminal pane on the right using :Term
