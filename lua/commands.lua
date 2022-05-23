086b1d4ad069d4cd0fda521e462afc46907338d2
Brian Weaver <bdrums@gmail.com>
2022-05-23 19:22:47 +0000

    FzfBufferDelete WIP

--- lua/commands.lua
+++ lua/commands.lua
@@ -11,3 +11,36 @@ create_cmd(
 )
 create_cmd("HashRocket", '<line1>,<line2>:norm ^csw"f:r a=>', { range = true })
 create_cmd("HashColon", '<line1>,<line2>:norm ^ds"f xxr:', { range = true })
+
+local fzf = require("fzf").fzf
+local helpers = require("fzf.helpers")
+
+coroutine.wrap(function()
+  local buffers = {}
+
+  local buffer_handles = vim.api.nvim_list_bufs()
+  for _, handle in pairs(buffer_handles) do
+    if vim.api.nvim_buf_is_loaded(handle) then
+      local buffer_name = vim.api.nvim_buf_get_name(handle)
+      table.insert(buffers, buffer_name)
+    end
+  end
+
+  local preview = helpers.choices_to_shell_cmd_previewer(function(items)
+    local current_buffer = items[1]
+    return "bat --style=numbers --color=always --pager=never -- " .. vim.fn.shellescape(current_buffer)
+  end)
+
+  local result = fzf(buffers, "--preview=" .. preview .. " --preview-window right:50% --multi")
+  if result then
+    vim.cmd("bdelete " .. table.concat(result, " "))
+  end
+end)()
+
+-- create_cmd(
+--   "FzfBufDelete",
+--   function()
+--     buffer_numbers = vim.api.nvim_list_bufs()
+--   end,
+--   {}
+-- )
