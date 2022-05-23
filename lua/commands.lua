a38ebdcbe9437b7c48c09774d92347062c420dba
Brian Weaver <bdrums@gmail.com>
2022-05-23 11:42:37 +0000

    Add some custom commands
    - Stashes
    - Branches
    - HashRocket
    - HashColon

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,4 +1,13 @@
-local cmd = vim.api.nvim_create_user_command
+local create_cmd = vim.api.nvim_create_user_command
 
-cmd("Branches", "lua require('fzf-git').branches()", {})
-cmd("Stashes", "lua require('fzf-git').stashes()", {})
+create_cmd("Branches", "lua require('fzf-git').branches()", {})
+create_cmd("Stashes", "lua require('fzf-git').stashes()", {})
+create_cmd(
+  "GutentagsClearCache",
+  function()
+    vim.fn.system("rm " .. vim.g.gutentags_cache_dir .. "/*")
+  end,
+  {}
+)
+create_cmd("HashRocket", '<line1>,<line2>:norm ^csw"f:r a=>', { range = true })
+create_cmd("HashColon", '<line1>,<line2>:norm ^ds"f xxr:', { range = true })
