4447b3b8fc3fd2812d0467654b953e2e484e541f
Brian Weaver <bdrums@gmail.com>
2022-06-20 13:21:38 -0400

    Remove plugin files that we don't need anymore

--- init.lua
+++ init.lua
@@ -11,8 +11,6 @@ require "keymaps"
 
 require "plugins/ale"
 require "plugins/alpha"
--- require "plugins/feline"
--- require "plugins/fzf"
 require "plugins/nvim-tree"
 require "plugins/treesitter"
 require "plugins/vim-test"
